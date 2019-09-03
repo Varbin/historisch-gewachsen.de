0852d30f84063c4417464d891094cd8bbc59d535
Simon Biewald <simon@fam-biewald.de>
2019-09-03 20:24:57 +0200

    drop tables before creation, recreate db in one commit

--- db_build.py
+++ db_build.py
@@ -4,17 +4,14 @@ import sqlite3
 def update_table(conn, table, quotefile):
     conn.execute(f"DROP TABLE IF EXISTS {table};")
     conn.execute(f"CREATE TABLE IF NOT EXISTS {table} (quote varchar UNIQUE);")
-    conn.commit()
 
+    print(table, end=": ")
     with open(quotefile) as quotes:
         for quote in quotes.readlines():
             quote = quote.strip()
-            try:
-                conn.execute(f"INSERT INTO {table} VALUES (?);", (quote,))
-            except sqlite3.IntegrityError:
-                conn.rollback()
-            else:
-                conn.commit()
+            print(f'"{quote}"', end='; ')
+            conn.execute(f"INSERT INTO {table} VALUES (?);", (quote,))
+    print()
 
     conn.commit()
 
@@ -22,7 +19,7 @@ def update_table(conn, table, quotefile):
 if __name__ == "__main__":
     conn = sqlite3.connect("quotes.sqlite")
 
-    conn.execute("CREATE TABLE IF NOT EXISTS suggestions (suggestion varchar, sugesttion_time date);")
+    conn.execute("CREATE TABLE IF NOT EXISTS suggestions (suggestion varchar, sugestion_time date);")
     conn.commit()
 
     update_table(conn, "quotes_de", "quotes_de.txt")
